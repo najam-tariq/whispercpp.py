d201308785043ae5145fbfb10f74efaa5e1da667
Najam Tariq <103676132+najam-tariq@users.noreply.github.com>
2024-07-12 03:15:47 -0400

    Update whispercpp.pyx

--- whispercpp.pyx
+++ whispercpp.pyx
@@ -1,133 +1,120 @@
-import sys
-import os
+#!python
+# cython: language_level=3
+
 import ffmpeg
 import numpy as np
 import requests
+import os
 from pathlib import Path
 
-class NoOutput:
-    def write(self, _):
-        pass
+MODELS_DIR = str(Path('~/.ggml-models').expanduser())
 
-    def flush(self):
-        pass
+cimport numpy as cnp
 
-original_stdout = sys.stdout
-original_stderr = sys.stderr
-sys.stdout = NoOutput()
-sys.stderr = NoOutput()
+cdef int SAMPLE_RATE = 16000
+cdef char* TEST_FILE = 'test.wav'
+cdef char* DEFAULT_MODEL = 'tiny'
+cdef char* LANGUAGE = b'en'
+cdef int N_THREADS = os.cpu_count()
 
-try:
-    MODELS_DIR = str(Path('~/.ggml-models').expanduser())
+MODELS = {
+    'ggml-tiny.bin': 'https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-tiny.bin',
+    'ggml-base.bin': 'https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-base.bin',
+    'ggml-small.bin': 'https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-small.bin',
+    'ggml-medium.bin': 'https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-medium.bin',
+    'ggml-large.bin': 'https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-large.bin',
+}
 
-    cimport numpy as cnp
+def model_exists(model):
+    return os.path.exists(Path(MODELS_DIR).joinpath(model))
 
-    cdef int SAMPLE_RATE = 16000
-    cdef char* TEST_FILE = 'test.wav'
-    cdef char* DEFAULT_MODEL = 'tiny'
-    cdef char* LANGUAGE = b'en'
-    cdef int N_THREADS = os.cpu_count()
+def download_model(model):
+    if model_exists(model):
+        return
 
-    MODELS = {
-        'ggml-tiny.bin': 'https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-tiny.bin',
-        'ggml-base.bin': 'https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-base.bin',
-        'ggml-small.bin': 'https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-small.bin',
-        'ggml-medium.bin': 'https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-medium.bin',
-        'ggml-large.bin': 'https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-large.bin',
-    }
-
-    def model_exists(model):
-        return os.path.exists(Path(MODELS_DIR).joinpath(model))
-
-    def download_model(model):
-        if model_exists(model):
-            return
-
-        url = MODELS[model]
-        r = requests.get(url, allow_redirects=True)
-        os.makedirs(MODELS_DIR, exist_ok=True)
-        with open(Path(MODELS_DIR).joinpath(model), 'wb') as f:
-            f.write(r.content)
+    url = MODELS[model]
+    r = requests.get(url, allow_redirects=True)
+    os.makedirs(MODELS_DIR, exist_ok=True)
+    with open(Path(MODELS_DIR).joinpath(model), 'wb') as f:
+        f.write(r.content)
 
 
-    cdef cnp.ndarray[cnp.float32_t, ndim=1, mode="c"] load_audio(bytes file, int sr = SAMPLE_RATE):
-        try:
-            out = (
-                ffmpeg.input(file, threads=0)
-                .output(
-                    "-", format="s16le",
-                    acodec="pcm_s16le",
-                    ac=1, ar=sr
-                )
-                .run(
-                    cmd=["ffmpeg", "-nostdin"],
-                )
-            )[0]
-        except:
-            raise RuntimeError(f"File '{file}' not found")
+cdef cnp.ndarray[cnp.float32_t, ndim=1, mode="c"] load_audio(bytes file, int sr = SAMPLE_RATE):
+    try:
+        out = (
+            ffmpeg.input(file, threads=0)
+            .output(
+                "-", format="s16le",
+                acodec="pcm_s16le",
+                ac=1, ar=sr
+            )
+            .run(
+                cmd=["ffmpeg", "-nostdin"],
+                capture_stdout=True,
+                capture_stderr=True
+            )
+        )[0]
+    except:
+        raise RuntimeError(f"File '{file}' not found")
 
-        cdef cnp.ndarray[cnp.float32_t, ndim=1, mode="c"] frames = (
-            np.frombuffer(out, np.int16)
-            .flatten()
-            .astype(np.float32)
-        ) / pow(2, 15)
+    cdef cnp.ndarray[cnp.float32_t, ndim=1, mode="c"] frames = (
+        np.frombuffer(out, np.int16)
+        .flatten()
+        .astype(np.float32)
+    ) / pow(2, 15)
 
-        return frames
+    return frames
 
-    cdef whisper_full_params default_params() nogil:
-        cdef whisper_full_params params = whisper_full_default_params(
-            whisper_sampling_strategy.WHISPER_SAMPLING_GREEDY
-        )
-        params.translate = False
-        params.language = <const char *> LANGUAGE
-        n_threads = N_THREADS
-        return params
-
-
-    cdef class Whisper:
-        cdef whisper_context * ctx
-        cdef whisper_full_params params
-
-        def __init__(self, model=DEFAULT_MODEL, pb=None, buf=None):
-            
-            model_fullname = f'ggml-{model}.bin'
-            download_model(model_fullname)
-            model_path = Path(MODELS_DIR).joinpath(model_fullname)
-            cdef bytes model_b = str(model_path).encode('utf8')
-            
-            if buf is not None:
-                self.ctx = whisper_init_from_buffer(buf, buf.size)
-            else:
-                self.ctx = whisper_init_from_file(model_b)
-            
-            self.params = default_params()
+cdef whisper_full_params default_params() nogil:
+    cdef whisper_full_params params = whisper_full_default_params(
+        whisper_sampling_strategy.WHISPER_SAMPLING_GREEDY
+    )
+    params.translate = False
+    params.language = <const char *> LANGUAGE
+    n_threads = N_THREADS
+    return params
 
 
-        def __dealloc__(self):
-            whisper_free(self.ctx)
+cdef class Whisper:
+    cdef whisper_context * ctx
+    cdef whisper_full_params params
 
-        def transcribe(self, filename=TEST_FILE):
-            if (type(filename) == np.ndarray) :
-                temp = filename
-            
-            elif (type(filename) == str) :
-                temp = load_audio(<bytes>filename)
-            else :
-                temp = load_audio(<bytes>TEST_FILE)
-
-            
-            cdef cnp.ndarray[cnp.float32_t, ndim=1, mode="c"] frames = temp
-
-            return whisper_full(self.ctx, self.params, &frames[0], len(frames))
+    def __init__(self, model=DEFAULT_MODEL, pb=None, buf=None):
         
-        def extract_text(self, int res):
-            if res != 0:
-                raise RuntimeError
-            cdef int n_segments = whisper_full_n_segments(self.ctx)
-            return [
-                whisper_full_get_segment_text(self.ctx, i).decode() for i in range(n_segments)
-            ]
+        model_fullname = f'ggml-{model}.bin'
+        download_model(model_fullname)
+        model_path = Path(MODELS_DIR).joinpath(model_fullname)
+        cdef bytes model_b = str(model_path).encode('utf8')
+        
+        if buf is not None:
+            self.ctx = whisper_init_from_buffer(buf, buf.size)
+        else:
+            self.ctx = whisper_init_from_file(model_b)
+        
+        self.params = default_params()
 
-finally:
-    sys.stdout = original_stdout
-    sys.stderr = original_stderr
+
+    def __dealloc__(self):
+        whisper_free(self.ctx)
+
+    def transcribe(self, filename=TEST_FILE):
+        if (type(filename) == np.ndarray) :
+            temp = filename
+        
+        elif (type(filename) == str) :
+            temp = load_audio(<bytes>filename)
+        else :
+            temp = load_audio(<bytes>TEST_FILE)
+
+        
+        cdef cnp.ndarray[cnp.float32_t, ndim=1, mode="c"] frames = temp
+
+        return whisper_full(self.ctx, self.params, &frames[0], len(frames))
+    
+    def extract_text(self, int res):
+        if res != 0:
+            raise RuntimeError
+        cdef int n_segments = whisper_full_n_segments(self.ctx)
+        return [
+            whisper_full_get_segment_text(self.ctx, i).decode() for i in range(n_segments)
+        ]
